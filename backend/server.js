5e0a1c7e02a911a2d69cf17d4f57ed6049462c57
@Ankit-kumar <142428226+Ankitgitkumar@users.noreply.github.com>
2024-07-04 22:37:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,27 +44,28 @@ app.post('/api/movies', async (req, res) => {
 });
 
  
+// app.get('/api/movies', async (req, res) => {
+//   try {
+//     const movies = await Movie.find();
+//     res.status(200).json(movies);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// });
+
 app.get('/api/movies', async (req, res) => {
   try {
-    const movies = await Movie.find();
-    res.status(200).json(movies);
+    const movie = await Movie.findOne({ title: req.params.title });
+    if (movie) {
+      res.status(200).send(movie);
+    } else {
+      res.status(404).send({ message: 'Movie not found' });
+    }
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).send(error);
   }
 });
 
-// app.get('/api/movies', async (req, res) => {
-//   try {
-//     const movie = await Movie.findOne({ title: req.params.title });
-//     if (movie) {
-//       res.status(200).send(movie);
-//     } else {
-//       res.status(404).send({ message: 'Movie not found' });
-//     }
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// });
 
 
 // app.get('/api/trailer', async (req, res) => {
